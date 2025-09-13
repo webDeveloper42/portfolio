d9ca94e1e43a3490383a12e8b9ed9d36cf8557c7
webDeveloper42 <rfrias361@gmail.com>
2025-09-13 04:28:11 -0400

    503 page and some random changes

---
+++ src/app/app-header/app-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-header',
+  imports: [],
+  templateUrl: './app-header.html',
+  styleUrl: './app-header.css'
+})
+export class AppHeader {
+
+}

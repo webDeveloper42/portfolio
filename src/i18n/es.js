2002596a6aed10d0eabd986c88cfb71afaa4782a
webDeveloper42 <rfrias361@gmail.com>
2026-03-23 19:26:37 -0400

    Updates

---
+++ src/i18n/es.js
@@ -0,0 +1,17 @@
+const esData = {
+  name: `webDeveloper42 / Raymond F. A.`,
+  title: `Ingeniero de Software`,
+  email: `rfrias1869@gmail.com`,
+  jobInterests: `Buscando trabajar con compañías en Japon`,
+  projects: [
+    {
+      img: `#`,
+      imgAlt: `Proyecto Spots`,
+      title: `Spots`,
+      description: `Una aplicación demo para compartir fotos que muestra las fotos de un usuario.`,
+      tags: [`HTML5`, `CSS3`, `JavaScript`, `RESTful API`, `OOP`],
+      link: `https://webdeveloper42.github.io/se_project_spots/`,
+    },
+  ],
+};
+export { esData };
---
+++ src/i18n/ja.js
@@ -0,0 +1,17 @@
+const jaData = {
+  name: `webDeveloper42 / Raymond F. A.`,
+  title: `ソフトウェアエンジニア`,
+  email: `rfrias1869@gmail.com`,
+  jobInterests: `日本の企業と一緒に働くことを希望しています`,
+  projects: [
+    {
+      img: `#`,
+      imgAlt: `Spotsプロジェクト`,
+      title: `Spots`,
+      description: `ユーザーの写真を紹介するデモ写真共有アプリです。`,
+      tags: [`HTML5`, `CSS3`, `JavaScript`, `RESTful API`, `OOP`],
+      link: `https://webdeveloper42.github.io/se_project_spots/`,
+    },
+  ],
+};
+export { jaData };

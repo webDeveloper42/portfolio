f1c50355f007b24cfda2a62816315eb61abdd24a
webDeveloper42 <rfrias361@gmail.com>
2026-04-25 16:35:33 -0400

    Updates

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -5,12 +5,12 @@ const esData = {
   jobInterests: `Estoy buscando trabajar con empresas en Japón`,
   projects: [
     {
-      img: `./src/assets/WTWRProject.png`,
-      imgAlt: `Una captura de pantalla del proyecto WTWR`,
-      title: `WTWR`,
-      description: `Una aplicación del clima que te muestra el clima actual en tu ubicación y te sugiere qué ponerte.`,
-      tags: [`React`, `CSS3`, `JavaScript`, `RESTful API`, `Geolocation API`],
-      link: `https://webdeveloper42.github.io/se_project_react/`,
+      img: `./src/assets/discover-yourself.png`,
+      imgAlt: `Una captura de pantalla del proyecto Discover Yourself`,
+      title: `Discover Yourself`,
+      description: `Una aplicación espiritual en vivo y monetizada para usuarios y creadores`,
+      tags: [`MERN`, `FullStack`],
+      link: `https://discover-yourself.onrender.com/`,
     },
     {
       img: `./src/assets/ShrineFinderDB.png`,
@@ -19,14 +19,6 @@ const esData = {
       description: `Una aplicación que lista santuarios en una base de datos de Japón`,
       tags: [`HTML5`, `CSS3`, `JavaScript`, `Database`],
       link: `https://webdeveloper42.github.io/shrine-database/`,
-    },
-    {
-      img: `./src/assets/discover-yourself.png`,
-      imgAlt: `Una captura de pantalla del proyecto Discover Yourself`,
-      title: `Discover Yourself`,
-      description: `Una aplicación espiritual en vivo y monetizada para usuarios y creadores`,
-      tags: [`MERN`, `FullStack`],
-      link: `https://discover-yourself.onrender.com/`,
     },
   ],
 };

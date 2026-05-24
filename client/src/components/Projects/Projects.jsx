ef603badd6152b98e9b4449c3da8d5bd9468d34d
webDeveloper42 <rfrias361@gmail.com>
2026-05-24 12:17:53 -0400

    Feature single visible project

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -2,13 +2,17 @@ import ProjectCard from '../ProjectCard/ProjectCard.jsx';
 import './Projects.css';
 
 function Projects({ projects, t }) {
+  const listClassName = `projects__list${
+    projects.length === 1 ? ' projects__list--single' : ''
+  }`;
+
   return (
     <section className="projects">
       <div className="projects__header">
         <h2 className="projects__heading">{t.ventures}</h2>
         <p className="projects__subheading">{t.venturesSubheading}</p>
       </div>
-      <ul className="projects__list">
+      <ul className={listClassName}>
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
